27ce0da13a7ada9ab97b55fff4bc89d315a372a0
yzx <you@example.com>
2018-07-12 09:37:48 +0800

    1

--- src/test/java/com/young/mvcwebdemo/MvcwebDemoApplicationTests.java
+++ src/test/java/com/young/mvcwebdemo/MvcwebDemoApplicationTests.java
@@ -1,12 +1,8 @@
 package com.young.mvcwebdemo;
 
-import com.young.mvcwebdemo.web.dao.UserRepo;
-import com.young.mvcwebdemo.web.domian.Dog;
-import com.young.mvcwebdemo.web.domian.User;
 import org.joda.time.DateTime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -17,29 +13,48 @@ import java.util.Random;
 public class MvcwebDemoApplicationTests {
 
 
-    @Autowired
-    private UserRepo userRepo;
 
-    @Test
-    public void contextLoads() {
-        //保存用户
-        User user = new User();
-        user.setName("ZhaoLei");
-        user.setAge(99);
-        //dog
-        Dog dog = new Dog();
-        dog.setId("12312");
-        dog.setName("旺财");
-        dog.setFood("bone");
-        user.setDog(dog);
-        userRepo.save(user);
-        System.out.println(user);
-    }
-
+//
+//    @Autowired
+//    private UserRepo userRepo;
+//
+//    @Test
+//    public void contextLoads() {
+//        //保存用户
+//        User user = new User();
+//        user.setName("ZhaoLei");
+//        user.setAge(99);
+//        //dog
+//        Dog dog = new Dog();
+//        dog.setId("12312");
+//        dog.setName("旺财");
+//        dog.setFood("bone");
+//        user.setDog(dog);
+//        userRepo.save(user);
+//        System.out.println(user);
+//    }
+//
     @Test
     public void testStr() {
         String dataStr = DateTime.now().toString("yyyyMMddHHmmss") + new Random().nextInt(1000);
         System.out.println(dataStr);
     }
+//
+//    @Test
+//    public void testUser() {
+//        User user = new User();
+//        user.setAge(12);
+//        user.setName("zzc");
+//
+//        Dog dog = user.getDog();
+//        dog.setId("001");
+//        dog.setName("小菜");
+//        dog.setFood("bone");
+//
+//        user.setDog(dog);
+//
+//        System.out.println(user);
+//
+//    }
 
 }
